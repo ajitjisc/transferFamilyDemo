ab2cfae3dd515a102e71206efc004dc929bb5123
ajitjisc <ajit.chandran@jisc.ac.uk>
2024-09-27 19:02:36 +0100

    added fourth s3 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,13 @@ resource "aws_s3_bucket" "home_bucket_2" {
   bucket = "my-tf-test-bucket-2-${var.aws_profile}"
 }
 
-# Second S3 Bucket
+# Third S3 Bucket
+resource "aws_s3_bucket" "home_bucket_3" {
+  bucket = "my-tf-test-bucket-3-${var.aws_profile}"
+}
+
+
+# fourth S3 Bucket
 resource "aws_s3_bucket" "home_bucket_3" {
   bucket = "my-tf-test-bucket-3-${var.aws_profile}"
 }
