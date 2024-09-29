516a84332329d7bbe2415955979f4b8a37995f11
ajitjisc <ajit.chandran@jisc.ac.uk>
2024-09-29 15:25:13 +0100

    added prod pipeline

--- codepipeline/main.tf
+++ codepipeline/main.tf
@@ -192,7 +192,7 @@ resource "aws_s3_bucket_versioning" "pipeline_artifacts_versioning" {
 }
 
 
-# CodePipeline setup
+# CodePipeline setup for Dev
 resource "aws_codepipeline" "transfer_family_pipeline" {
   name     = "TransferFamilyPipeline"
   role_arn = aws_iam_role.codepipeline_role.arn
@@ -276,6 +276,131 @@ resource "aws_codepipeline" "transfer_family_pipeline" {
       }
     }
   }
+}
+
+# CodePipeline setup for Prod
+resource "aws_codepipeline" "transfer_family_pipeline_prod" {
+  name     = "TransferFamilyPipelineProd"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    type     = "S3"
+    location = aws_s3_bucket.pipeline_artifacts.bucket
+  }
+
+  # Source stage using GitHub version 2 with CodeStar Connection
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        ConnectionArn   = "arn:aws:codestar-connections:eu-west-2:492883160621:connection/c5ac251e-4e89-4e22-be0b-10e48f21a273"
+        FullRepositoryId = "ajitjisc/transferFamilyDemo"
+        BranchName      = ""   
+        DetectChanges    = "true"
+      }
+
+      # Define a namespace for the source stage variables
+      namespace = "SourceVariables"
+    }
+  }
+
+  # Terraform stage
+  stage {
+    name = "Terraform"
+
+    action {
+      name             = "TerraformBuild"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.terraform_project.name
+        EnvironmentVariables = jsonencode([
+          {
+            name  = "ENV"
+            value = "#{SourceVariables.BranchName}"
+            type  = "PLAINTEXT"
+          }
+        ])
+      }
+    }
+  }
+
+  # Serverless stage
+  stage {
+    name = "DeployServerless"
+
+    action {
+      name             = "ServerlessDeploy"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["build_output"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.serverless_project.name
+        EnvironmentVariables = jsonencode([
+          {
+            name  = "ENV"
+            value = "#{SourceVariables.BranchName}"
+            type  = "PLAINTEXT"
+          }
+        ])
+      }
+    }
+  }
+}
+
+# Trigger for CodePipelineProd
+resource "aws_codepipeline_webhook" "codepipeline_webhook_prod" {
+  name            = "TransferFamilyPipelineProdWebhook"
+  target_action   = "Source"
+  target_pipeline = aws_codepipeline.transfer_family_pipeline_prod.name
+
+  authentication = "GITHUB_HMAC"
+
+  authentication_configuration {
+    secret_token = var.github_webhook_secret
+  }
+
+  filter {
+    json_path    = "$.ref"
+    match_equals = "refs/heads/feature*"
+  }
+
+  tags = {
+    Environment = "Prod"
+  }
+}
+
+# Add permissions for CodePipeline to invoke the webhook for Prod
+resource "aws_iam_role_policy" "webhook_invocation_policy_prod" {
+  name = "WebhookInvocationPolicyProd"
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "codepipeline:StartPipelineExecution",
+        Resource = aws_codepipeline.transfer_family_pipeline_prod.arn
+      }
+    ]
+  })
 }
 
 # Trigger for CodePipeline
