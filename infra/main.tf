a349493df92009869a105520adda0c3bf43883e3
ajitjisc <ajit.chandran@jisc.ac.uk>
2024-10-04 00:43:14 +0100

    new_7 typo

--- infra/main.tf
+++ infra/main.tf
@@ -20,7 +20,7 @@ resource "aws_s3_bucket" "home_bucket_4" {
   bucket = "my-tf-test-bucket-4-${var.aws_profile}"
 }
 
-# fifthNew S3 Bucket
+# six S3 Bucket
 resource "aws_s3_bucket" "home_bucket_5" {
   bucket = "my-tf-test-bucket-5-${var.aws_profile}"
 }
